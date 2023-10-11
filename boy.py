9d9608b71872f7b72f29889772b89ea3b36894c2
yunseochoi(home) <jamie0730@naver.com>
2023-10-11 22:46:37 +0900

    Idle상태에서 a키를 누르면 autorun 기능 추가 및 자동 방향 기능과 autorun 5초 이상 경과 시 Idle로 돌아가는 기능 구현

--- boy.py
+++ boy.py
@@ -1,4 +1,4 @@
-from pico2d import load_image, SDLK_SPACE, get_time, SDL_KEYUP, SDLK_RIGHT, SDL_KEYDOWN, SDLK_LEFT
+from pico2d import load_image, SDLK_SPACE, get_time, SDL_KEYUP, SDLK_RIGHT, SDL_KEYDOWN, SDLK_LEFT, SDLK_a
 
 
 def space_down(e):
@@ -25,6 +25,41 @@ def left_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 
 
+def auto_run(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_a
+
+
+class AutoRun:
+    @staticmethod
+    def enter(boy, e):
+        if auto_run(e):
+            boy.dir, boy.action = 1, 1
+            print('auto')
+
+    @staticmethod
+    def exit(boy, e):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        if boy.x > 800:
+            boy.dir = -1
+        elif boy.x < 0:
+            boy.dir = 1
+        boy.x += boy.dir * 5
+
+        if get_time() - boy.wait_time > 5:
+            boy.state_machine.handle_event(('TIME_OUT', 0))
+
+    @staticmethod
+    def draw(boy):
+        if boy.dir == 1:
+            boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+        elif boy.dir == -1:
+            boy.image.clip_composite_draw(boy.frame * 100, boy.action * 100, 100, 100, 0, 'h', boy.x, boy.y, 100, 100)
+
+
 class Run:
 
     @staticmethod
@@ -34,6 +69,7 @@ class Run:
         elif left_down(e) or right_up(e):
             boy.dir, boy.action = -1, 0
 
+        boy.wait_time = get_time()
     @staticmethod
     def exit(boy, e):
         pass
@@ -107,9 +143,10 @@ class StateMachine:
         self.boy = boy
         self.cur_state = Idle
         self.transitions = {
-            Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep},
+            Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep, auto_run: AutoRun},
             Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
-            Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle}
+            Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle},
+            AutoRun: {auto_run: AutoRun, time_out: Sleep},
         }
 
     def handle_event(self, e):
